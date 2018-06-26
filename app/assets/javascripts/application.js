8759590e868cd2aeb4a098993a31bdf50c16716a
lein <lein.davir@gmail.com>
2018-06-26 17:40:35 -0500

    some styles to form for

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require twitter/bootstrap
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
